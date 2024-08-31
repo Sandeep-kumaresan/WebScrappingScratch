934da4d9bd821301989a8d5307f2c72754724fda
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-08-31 20:52:30 +0530

    Learning

--- Scrapping.py
+++ Scrapping.py
@@ -11,3 +11,13 @@ tag = soup.header.div.a.button.span
 print(tag.string)
 price = soup.find("h4",{"class":"price float-end card-title pull-right"})
 print(price.string)
+dec = soup.find("p",class_="description card-text")
+print(dec.string)
+print("--------------------------------------------------------------------------------")
+priceall = soup.find_all("h4",class_="price float-end card-title pull-right")
+# for i in priceall:
+#     print(i.text)
+descall = soup.find_all("p",class_="description card-text")
+print(descall[3].text)
+#Strfind = soup.find_all(string="Pavilion")
+#print(Strfind)
